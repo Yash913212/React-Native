07b2e1ed166b35490031c2f6f2b5331981a5ab3d
Yash <yash@yash.yash>
2026-02-13 22:03:21 +0530

    Added RN Paper

--- Demo/App.js
+++ Demo/App.js
@@ -1,11 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, FlatList} from 'react-native';
+import Photo1 from "./assets/15.webp";
+import Photo2 from "./assets/AI_Image.webp";
+import Photo3 from "./assets/Ai_Img.webp";
+import Photo4 from "./assets/Ai_Img2.webp";
+import Photo5 from "./assets/Ai_Img3.webp";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.container}>w
+        <FlatList
+
+         />
     </View>
   );
 }
